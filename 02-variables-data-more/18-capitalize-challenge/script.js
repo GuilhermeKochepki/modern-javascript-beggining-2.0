7df889e013fc1135cd4dcce70f2960593443dced
Guilherme Kochepki <gui.kochepki@gmail.com>
2023-06-19 20:28:53 -0300

    Resolvendo o desafio de cappitalize

---
+++ 02-variables-data-more/18-capitalize-challenge/script.js
@@ -0,0 +1,7 @@
+//Faça uma operação de Cappitalize para uma string de uma palavra
+
+const myString = 'developer';
+
+const myNewString = myString.substring(0,1).toUpperCase() + myString.substring(1);
+
+console.log(myNewString);
